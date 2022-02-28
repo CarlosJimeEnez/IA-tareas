f37534e570ca2bcf172a1b34d7c2c23996b63078
CarlosJimeEnez <jjimenezpena7@gmail.com>
2022-02-28 02:04:23 -0600

    Completed: Trian, Trap

--- tarea_3/Main.m
+++ tarea_3/Main.m
@@ -3,6 +3,13 @@ clear
 
 finalizado = "n"; 
 while finalizado ~= "S" 
+    disp("Conjuntos: ")
+    disp("Triangular")
+    disp("Trapezoidal")
+    disp("Gaussiana")
+    disp("pseudo-exponencial")
+    disp("Gamma1")
+    disp("Gamma2")
     input("Conjunto: ") %#ok<*NOPTS> 
     finalizado = input("Acabar S/n: ", "s"); 
 end
