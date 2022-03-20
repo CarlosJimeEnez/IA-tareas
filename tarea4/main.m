82ef83a89d87dc0e8e304d591eb34005713fc380
CarlosJimeEnez <jjimenezpena7@gmail.com>
2022-03-20 03:23:37 -0600

    finish

--- tarea4/main.m
+++ tarea4/main.m
@@ -3,17 +3,17 @@ clear all
 
 ts = vector_tiempo(0.001, 0, 10); 
 %Parametros de A: 
-disp('Parmateros de A:  k1 = 1, m1 = 5')
+disp('(Gaussiana) Parmateros de A:  k1 = 1, m1 = 5')
 k1 = 1; m1 = 5; 
 %disp('Intervalo de la grafica = [2.5, 8]')
 
 %Paramtetros de B:
-disp('Parametros de B: k1 = 1, m1 = 5')
+disp('(Pseudo exponencial)Parametros de B: k1 = 1, m1 = 5')
 k2 = 1; m2 = 5; 
 %disp('Intervalo de la grafica: [2.5, 10]')
 
 %Paramtetros de A':
-disp('Parametros de B: k1 = 3, m1 = 5')
+disp('(Pseudo exponencial)Parametros de B: k1 = 3, m1 = 5')
 k3 = 2; m3 = 5; 
 %disp('Intervalo de la grafica: [2.5, 10]')
 
@@ -44,8 +44,16 @@ for j = 1:cols
 end
 
 %% R(x,y)
+
 fun_r = zeros(rows, cols); 
-y = zeros(rows, cols); 
+% fun_r guarda los valores de la funcion R() = min(1, 1 - A + B)
+
+% y guarda los valores de la forma: 
+% %Salida y(i,j):
+%     y1        y2             y++
+% x1  1.0000    0.9000         0         0
+% x2  1.0000    0.4000         0         0
+y = zeros(cols, cols); 
 
 %Donde i es el recorrido por A y j por B
 for i = 1:cols
@@ -57,13 +65,20 @@ end
 
 
 %% Bprima: 
-b_min = zeros(rows, cols); 
+% Buscamos el valor minimo de la forma: min(A', R(x', y))
+b_min = zeros(cols, cols); 
+
+
 for i = 1:cols
     for j = 1:cols
-        b_min(i, j) = min(a_dif_pseudo(:,j), y(j,i)) ;
+        b_min(i, j) = min(a_dif_pseudo(:,j), y(j,i));
     end
 end
 
+% b_min guarda los valores del modo: 
+%b_min   = [minimos de B(y1), 
+%          minimos de B(y2), 
+%           ...]
 
 for j = 1:cols
     b_dif_pseudo(:,j) = max( b_min(j,:));
@@ -72,7 +87,7 @@ end
 
 %% Display graficas: 
 %
-tiledlayout(3,1)
+tiledlayout(4,1)
 nexttile
 plot(ts, a_gaussiana)
 title("Grafica A")
@@ -85,6 +100,8 @@ nexttile
 plot(ts, a_dif_pseudo)
 title("Grafica A'")
 
-
+nexttile
+plot(ts, b_dif_pseudo)
+title("Grafica B'")
 
 
